a22d9b3e57f7f65f816f48fff68cd34ae4ac2197
ehl7 <ehlockard@gmail.com>
2021-04-08 15:27:22 -0700

    changed it to functio that calculates how many km away given lightyears

--- inc02b.R
+++ inc02b.R
@@ -1,6 +1,12 @@
-c <- 299792458
-lightMinute <- c*60
-lightHour <- lightMinute*60
-lightDay <- lightHour*24
-lightYear <- lightDay*365.2422
-cat("length of light year:", lightYear, "\n") 
+
+calc_lightYear <- function(ly) {
+  c <- 299792458
+  lightMinute <- c*60
+  lightHour <- lightMinute*60
+  lightDay <- lightHour*24
+  lightYear <- lightDay*365.2422
+  #cat("length of light year:", lightYear, "\n")
+  ly * lightYear 
+}
+
+print(calc_lightYear(55))
